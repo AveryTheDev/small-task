5b6a55a4380841eb3f27b77149aade50836e4740
Avery Brown <Avery@Macintosh-406c8f1c1a52.local>
2020-01-10 10:56:37 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Small Task
+
+This application was created with NextJS and Material UI with the challenge of only styling with the Box component and one img tag.
+
+The rules were that no classNames, style prop or external CSS could be used to style the component. Only the properties of Box.
--- pages/index.js
+++ pages/index.js
@@ -1,88 +1,104 @@
-import React from 'react'
-import Head from 'next/head'
-import Nav from '../components/nav'
+import React from "react";
+import { styled } from "@material-ui/core/styles";
+import {
+  borders,
+  compose,
+  display,
+  flexbox,
+  palette,
+  shadows,
+  sizing,
+  spacing,
+  typography
+} from "@material-ui/system";
+
+const Box = styled("div")(
+  compose(
+    borders,
+    display,
+    flexbox,
+    palette,
+    shadows,
+    sizing,
+    spacing,
+    typography
+  )
+);
+const Image = styled("img")(compose(borders, flexbox, sizing));
+
+const IMAGE_SRC = `https://images.unsplash.com/photo-1567742364484-024e82826777?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&h=400&q=100`;
+
+const width = {
+  // The grey container width should change on breakpoint (container is also a Box)
+  xs: "100%",
+  sm: 600,
+  md: 960,
+  lg: 1280,
+  xl: 1680
+};
+const maxImageHeight = {
+  xs: "320px",
+  md: "200px"
+};
+const maxImageWidth = {
+  xs: "320px",
+  md: "200px"
+};
+const cardWidth = {
+  xs: "320px",
+  md: "450px"
+};
 
 const Home = () => (
-  <div>
-    <Head>
-      <title>Home</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+  <Box maxWidth="100%" display="flex" justifyContent="center">
+    <Box
+      display="flex"
+      justifyContent="center"
+      width={width}
+      m={1}
+      p={{ xs: 4, sm: 5, md: 6 }}
+      //need to lighten to make clear that the box shadow is present
+      bgcolor="success.main"
+    >
+      <Box
+        bgcolor="white"
+        boxShadow={3}
+        borderRadius={8}
+        display="flex"
+        flexDirection={{ xs: "column", md: "row" }}
+        overflow="hidden"
+        width={cardWidth}
+      >
+        <Image
+          maxHeight={maxImageHeight}
+          maxWidth={maxImageWidth}
+          src={IMAGE_SRC}
+          alt="a relaxed lizard"
+        ></Image>
+        <Box p={{ xs: 2, sm: 3 }}>
+          <Box
+            fontSize={26}
+            fontWeight="fontWeightMedium"
+            mb={{ xs: 2, sm: 2, md: 2 }}
+          >
+            Lizard
+          </Box>
+          <Box fontSize={14} color="gray" mb={{ xs: 4 }} lineHeight={1.2}>
+            {" "}
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Box>
+          <Box
+            fontSize={15}
+            fontWeight="fontWeightMedium"
+            color="secondary.main"
+          >
+            LEARN MORE
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  </Box>
+);
 
-    <Nav />
-
-    <div className="hero">
-      <h1 className="title">Welcome to Next.js!</h1>
-      <p className="description">
-        To get started, edit <code>pages/index.js</code> and save to reload.
-      </p>
-
-      <div className="row">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Learn more about Next.js in the documentation.</p>
-        </a>
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Next.js Learn &rarr;</h3>
-          <p>Learn about Next.js by following an interactive tutorial!</p>
-        </a>
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Find other example boilerplates on the Next.js GitHub.</p>
-        </a>
-      </div>
-    </div>
-
-    <style jsx>{`
-      .hero {
-        width: 100%;
-        color: #333;
-      }
-      .title {
-        margin: 0;
-        width: 100%;
-        padding-top: 80px;
-        line-height: 1.15;
-        font-size: 48px;
-      }
-      .title,
-      .description {
-        text-align: center;
-      }
-      .row {
-        max-width: 880px;
-        margin: 80px auto 40px;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-      }
-      .card {
-        padding: 18px 18px 24px;
-        width: 220px;
-        text-align: left;
-        text-decoration: none;
-        color: #434343;
-        border: 1px solid #9b9b9b;
-      }
-      .card:hover {
-        border-color: #067df7;
-      }
-      .card h3 {
-        margin: 0;
-        color: #067df7;
-        font-size: 18px;
-      }
-      .card p {
-        margin: 0;
-        padding: 12px 0 0;
-        font-size: 13px;
-        color: #333;
-      }
-    `}</style>
-  </div>
-)
-
-export default Home
+export default Home;
